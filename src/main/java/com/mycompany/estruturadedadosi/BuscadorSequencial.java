9ab538c9edfd5e80c77d00fc97f9e1612d52fc91
Musta-eideh <mustafaeideh100@outlook.com>
2023-03-11 13:28:51 -0300

    BuscadorSequencial

--- src/main/java/com/mycompany/estruturadedadosi/BuscadorSequencial.java
+++ src/main/java/com/mycompany/estruturadedadosi/BuscadorSequencial.java
@@ -3,6 +3,34 @@ package com.mycompany.estruturadedadosi;
 public class BuscadorSequencial {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int vet[] = new int[10];
+        
+        for(int i = 0; i < vet.length; i++){
+            vet[i] = i * 2;
+            System.out.println("vet " + vet[i]);
+        }
+        int pos = BuscaNumero(vet, 100);
+        if (pos != 0) {
+            System.out.println("Número encontrado na posição.");
+        }else{
+            System.out.println("Número não encontrado na posição.");
+        }
+    }
+    
+    public static int BuscaNumero(int v[], int vlrPesquisado){
+        boolean achou = false;
+        int pos = 0;
+        for (int i = 0; i < v.length; i++){
+            if (v[i] == vlrPesquisado){
+                achou = true;
+                pos = i;
+            }
+        }
+        
+        if (achou == true) {
+            return pos;
+        } else{
+            return pos;
+        }
     }
 }
